6d3dde6c8f48bee377e70f5f63e44a5b78d2bdd0
Ryan Pavlik <rpavlik@iastate.edu>
2012-07-06 16:58:16 -0500

    Implement ErrorComputer

--- src/ErrorComputer.cpp
+++ src/ErrorComputer.cpp
@@ -21,8 +21,59 @@
 #include "ErrorComputer.h"
 
 // Library/third-party includes
-// - none
+#include <vrpn_Analog_Output.h>
+#include <vrpn_Tracker.h>
 
 // Standard includes
-// - none
+#include <iostream>
 
+ErrorComputer::ErrorComputer(vrpn_Tracker_Remote * tracker, vrpn_Analog_Output_Remote * output, int robot_sensor, int goal_sensor, size_t worldx, size_t worldz)
+	: _tracker(tracker)
+	, _output(output)
+	, _robotSensor(robot_sensor)
+	, _gotRobot(false)
+	, _positionRobot(3)
+	, _goalSensor(goal_sensor)
+	, _gotGoal(false)
+	, _positionGoal(3)
+	, _worldx(worldx)
+	, _worldz(worldz) {
+	_tracker->register_change_handler(this, &ErrorComputer::_handleRobot, _robotSensor);
+	_tracker->register_change_handler(this, &ErrorComputer::_handleGoal, _goalSensor);
+	_lastOutput[0] = 0;
+	_lastOutput[1] = 0;
+}
+
+ErrorComputer::~ErrorComputer() {
+	_tracker->unregister_change_handler(this, &ErrorComputer::_handleRobot, _robotSensor);
+	_tracker->unregister_change_handler(this, &ErrorComputer::_handleGoal, _goalSensor);
+}
+
+void VRPN_CALLBACK ErrorComputer::_handleRobot(void * userdata, const vrpn_TRACKERCB info) {
+	ErrorComputer * self = static_cast<ErrorComputer*>(userdata);
+	self->_gotRobot = true;
+	self->_positionRobot = std::valarray<double>(info.pos, 3);
+	//std::cout << "Robot: " << self->_positionRobot[0] << ", " << self->_positionRobot[1] << ", " << self->_positionRobot[2] << std::endl;
+}
+
+void VRPN_CALLBACK ErrorComputer::_handleGoal(void * userdata, const vrpn_TRACKERCB info) {
+	ErrorComputer * self = static_cast<ErrorComputer*>(userdata);
+	self->_gotGoal = true;
+	self->_positionGoal = std::valarray<double>(info.pos, 3);
+	//std::cout << "Goal: " << self->_positionGoal[0] << ", " << self->_positionGoal[1] << ", " << self->_positionGoal[2] << std::endl;
+}
+
+void ErrorComputer::operator()() {
+	if (_gotGoal && _gotRobot) {
+		std::valarray<double> error = _positionGoal - _positionRobot;
+
+		ChannelArray desiredOutput = {error[_worldx], error[_worldz]};
+		if (desiredOutput != _lastOutput) {
+			std::cout << "Requesting output " << desiredOutput[0] << ", " << desiredOutput[1] << std::endl;
+			_output->request_change_channels(CHANNELS, desiredOutput.data());
+			_lastOutput = desiredOutput;
+			_gotGoal = false;
+			_gotRobot = false;
+		}
+	}
+}
--- src/ErrorComputer.h
+++ src/ErrorComputer.h
@@ -24,9 +24,45 @@
 // - none
 
 // Library/third-party includes
-// - none
+#include <boost/noncopyable.hpp>
+#include <boost/array.hpp>
+#include <vrpn_Tracker.h>
 
 // Standard includes
-// - none
+#include <valarray>
+
+class vrpn_Analog_Output_Remote;
+
+class ErrorComputer : boost::noncopyable {
+	public:
+		enum {
+			CHANNELS = 2
+		};
+		typedef boost::array<vrpn_float64, CHANNELS> ChannelArray;
+		ErrorComputer(vrpn_Tracker_Remote * tracker, vrpn_Analog_Output_Remote * output, int robot_sensor = 0, int goal_sensor = 1, size_t worldx = 0, size_t worldz = 1);
+		~ErrorComputer();
+
+		/// Compute and send updated error, if applicable.
+		void operator()();
+	private:
+		static void VRPN_CALLBACK _handleRobot(void * userdata, const vrpn_TRACKERCB info);
+		static void VRPN_CALLBACK _handleGoal(void * userdata, const vrpn_TRACKERCB info);
+
+		vrpn_Tracker_Remote * _tracker;
+		vrpn_Analog_Output_Remote * _output;
+
+		int _robotSensor;
+		bool _gotRobot;
+		std::valarray<double> _positionRobot;
+
+		int _goalSensor;
+		bool _gotGoal;
+		std::valarray<double> _positionGoal;
+
+		size_t _worldx;
+		size_t _worldz;
+
+		ChannelArray _lastOutput;
+};
 
 #endif // INCLUDED_ErrorComputer_h_GUID_fa857175_0ca1_4b88_820a_f7d0b1233c46
