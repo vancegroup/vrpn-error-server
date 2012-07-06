ab01f6e7bb8291c61d9a7e2012325cf35136840a
Ryan Pavlik <rpavlik@iastate.edu>
2012-07-06 15:46:39 -0500

    Stub in new file

---
+++ src/ErrorComputer.h
@@ -0,0 +1,32 @@
+/** @file
+	@brief Header
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#ifndef INCLUDED_ErrorComputer_h_GUID_fa857175_0ca1_4b88_820a_f7d0b1233c46
+#define INCLUDED_ErrorComputer_h_GUID_fa857175_0ca1_4b88_820a_f7d0b1233c46
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+#endif // INCLUDED_ErrorComputer_h_GUID_fa857175_0ca1_4b88_820a_f7d0b1233c46
