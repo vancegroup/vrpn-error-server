607a02ce6edd4163aba8c6655d289fbc41359032
Ryan Pavlik <rpavlik@iastate.edu>
2012-10-05 14:21:25 -0500

    Start work on AppObject

---
+++ src/AppObject.cpp
@@ -0,0 +1,90 @@
+/**
+	@file
+	@brief Implementation
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// Internal Includes
+#include "AppObject.h"
+#include "CleanExit.h"
+#include "ReceiveEchoer.h"
+
+// Library/third-party includes
+#include <vrpn_Configure.h>
+
+// Standard includes
+#include <string>
+#include <cstdlib>
+
+extern const char * vrpn_MAGIC;
+
+AppObject::AppObject()
+	: _cmd("Send appropriate error commands to a serial-connected controller", ' ',
+	       "1.0 (using " + std::string(vrpn_MAGIC) + ")")
+	, _c(NULL)
+	, _port()
+	, _container()
+	, _interval(0.0) {
+
+	/// Let _cmd deal with its own mess.
+	_cmd.setExceptionHandling(true);
+	CleanExit::instance().registerHandlers();
+
+#ifdef _WIN32
+	WSADATA wsaData;
+	int status;
+	if ((status = WSAStartup(MAKEWORD(1, 1), &wsaData)) != 0) {
+		std::cerr << "WSAStartup failed with " << status << std::endl;
+		std::abort();
+	}
+	VERBOSE_MSG("WSAStartup completed");
+#endif // WIN32
+
+}
+
+void AppObject::parseAndBeginSetup(int argc, const char * const * argv) {
+	TCLAP::ValueArg<std::string> portname("p", "port", "serial port name", true, "", "serial port", _cmd);
+	TCLAP::ValueArg<long> baudrate("b", "baud", "baud rate", false, 115200, "baud rate", _cmd);
+	TCLAP::ValueArg<int> portnumval("n", "netport", "network port for VRPN to listen on (defaults to standard VRPN port)", false, vrpn_DEFAULT_LISTEN_PORT_NO, "port", _cmd);
+	TCLAP::ValueArg<double> msginterval("i", "interval", "milliseconds of interval between messages", false, 0, "ms", _cmd);
+
+	_cmd.parse(argc, argv);
+
+	VERBOSE_START("Creating server connection on port " << portnumval.getValue());
+	_c = vrpn_create_server_connection(portnumval.getValue());
+	_container.add(_c);
+	VERBOSE_DONE();
+
+
+	VERBOSE_START("Opening serial port " << portname.getValue() << " at a baudrate of " << baudrate.getValue());
+	_port.open(portname.getValue().c_str(), baudrate.getValue());
+	VERBOSE_DONE();
+
+	_interval = msginterval.getValue();
+}
+
+void AppObject::addReceiveEchoer() {
+	VERBOSE_START("Creating receive echoer");
+	_container.add(new ReceiveEchoer(_port));
+	VERBOSE_DONE();
+}
+
+void enterMainloop() {
+	while (!CleanExit::instance().exitRequested()) {
+		_container.mainloop();
+		vrpn_SleepMsecs(1);
+	}
+}
---
+++ src/AppObject.h
@@ -0,0 +1,86 @@
+/** @file
+	@brief Header
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#ifndef INCLUDED_AppObject_h_GUID_cf6e4611_d60e_40ec_893c_5d57c79a1779
+#define INCLUDED_AppObject_h_GUID_cf6e4611_d60e_40ec_893c_5d57c79a1779
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+#include <tclap/CmdLine.h>
+#include <vrpn_MainloopContainer.h>
+#include <vrpn_SerialPort.h>
+
+// Standard includes
+#include <iostream>
+#include <string>
+
+
+#define VERBOSE_START(X) std::cout << X << "..." << std::flush
+#define VERBOSE_MSG(X) std::cout << X << std::endl
+#define VERBOSE_DONE() std::cout << " done." << std::endl
+
+class vrpn_Connection;
+
+/// @brief An object to use for shared code between the various executables
+class AppObject {
+	public:
+		AppObject();
+		~AppObject();
+
+		void parseAndBeginSetup(int argc, const char * const * argv);
+
+		template<typename T>
+		void addToMainloop(T obj) {
+			_container.add(obj);
+		}
+
+		vrpn_Connection * getConnection() const {
+			return _c;
+		}
+
+		vrpn_SerialPort & getSerialPort() {
+			return _port;
+		}
+
+		template<typename Collection, typename MessageType>
+		void addBinaryCommandOutput(std::string const & devName) {
+			_container.add(new BinaryCommandOutput<Collection, MessageType>(devName.c_str(), _port, _c, _interval);
+		}
+
+		               template<int NumChannels, char CommandPrefix>
+		void addCommandOutput(std::string const & devName) {
+			_container.add(new CommandOutput<NumChannels, CommandPrefix>(devName.c_str(), _port, _c, _interval);
+		}
+
+		               void addReceiveEchoer();
+
+		void enterMainloop();
+	private:
+		TCLAP::CmdLine _cmd;
+		vrpn_Connection * _c;
+		vrpn_SerialPort _port;
+		vrpn_MainloopContainer _container;
+		double _interval;
+
+
+};
+
+#endif // INCLUDED_AppObject_h_GUID_cf6e4611_d60e_40ec_893c_5d57c79a1779
